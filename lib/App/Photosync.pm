1cc685f1fdbae99fda238f154c5cd7170bea151c
Lee Aylward <lee@laylward.com>
2012-10-29 18:50:49 -0500

    get uploads actually working

--- lib/App/Photosync.pm
+++ lib/App/Photosync.pm
@@ -1,39 +1,32 @@
 package App::Photosync;
 
 use v5.12;
+
 use Cwd;
 use File::Find;
-use Getopt::Long;
 use Mac::FSEvents;
 use AnyEvent;
-use AnyEvent::HTTP;
-use Digest::MD5 qw/md5_hex/;
-use Digest::HMAC_SHA1 qw/hmac_sha1_hex/;
+use AnyEvent::HTTP ();
+use Digest::MD5 qw/md5/;
+use Digest::HMAC_SHA1 qw/hmac_sha1/;
 use MIME::Base64 qw/encode_base64/;
 
 sub new {
-  my $class = shift;
-  my ($source, $dest, $secret, $key);
+  my ($class, %args) = @_;
 
-  GetOptions(
-    "source=s"   => \$source,
-    "dest=s"     => \$dest,
-    "secret=s"   => \$secret,
-    "key=s"      => \$key,
-  );
-
-  die "must provide source"   unless $source;
-  die "must provide dest"     unless $dest;
-  die "must provide aws keys" unless $secret and $key;
+  for (qw/source dest secret key/) {
+    die "$_ is required" unless defined $args{$_};
+  }
 
   return bless {
-    source   => Cwd::abs_path($source),
-    dest     => $dest,
+    source   => Cwd::abs_path($args{source}),
+    dest     => $args{dest},
+    aws      => [ @args{qw/key secret/} ],
     seen     => {},
     cv       => AE::cv,
     filter   => sub {
       my $f = shift;
-      -f $f && $f =~ /(?:jpg|png}gif)$/;
+      -f $f && $f =~ /(?:jpg|png|gif)$/i && (stat($f))[7] > 0;
     },
   }, $class;
 }
@@ -41,18 +34,15 @@ sub new {
 sub run {
   my $self = shift;
   $self->{cv}->begin;
+  $self->scan_source;
 
   say "watching $self->{source}";
   say "uploading to $self->{dest}";
 
-  $self->scan_source;
-
-  my $fs = Mac::FSEvents->new({
-    path => $self->{source},
-  });
+  my $fs = Mac::FSEvents->new({path => $self->{source}});
 
   my $io = AE::io $fs->watch, 0, sub {
-    $self->handle_event($_) for $fs->read_events
+    $self->handle_event;
   };
 
   my $sig = AE::signal INT => sub {
@@ -63,7 +53,7 @@ sub run {
 }
 
 sub handle_event {
-  my ($self, $event) = @_;
+  my $self = shift;
   my ($add, $del) = $self->scan_source;
   $self->upload_file($_) for @$add;
 }
@@ -91,31 +81,39 @@ sub upload_file {
   my ($self, $path) = @_;
 
   my $remote = $path;
-  $remote =~ s/^\Q$self->{source}\E//;
+  $remote =~ s/^\Q$self->{source}\E/$self->{dest}/;
+
+  say "uploading $path to $remote";
 
   my $body = do {
+    open(my $fh, '<', $path) or die "unable to read image: $!";
     local $/;
-    open my $fh, '<', $path
-      or die "unable to read image: $!";
     <$fh>;
   };
 
+  my ($key, $secret) = @{$self->{aws}};
   my %h = (
-    "Content-Md5" => md5_hex($body),
+    "Content-Md5" => encode_base64(md5($body), ""),
     "Content-Type" => "image/jpeg",
-    "Date" => scalar(localtime(time)),
+    "Date" =>  AnyEvent::HTTP::format_date time,
   );
 
-  my $url = "http://s3.amazonaws.com/$self->{dest}";
-  my $sig = hmac_sha1_hex join "\n", "PUT", @h{qw/Content-Md5 Content-Type Date/};
-  $h{Authorization} = join ":", "AWS $self->{key}", encode_base64 $sig;
+  my $sig = hmac_sha1
+    join("\n", "PUT", @h{qw/Content-Md5 Content-Type Date/}, $remote),
+    $secret;
+
+  $h{Authorization} = "AWS $key:".encode_base64($sig, "");
 
   $self->{cv}->begin;
 
-  http_request PUT => "$url/$remote", %h, sub {
-    my ($body, $headers) = @_;
-    $self->{cv}->end; 
-  };
+  AnyEvent::HTTP::http_request
+    PUT => "http://s3.amazonaws.com$remote",
+    headers => \%h,
+    body => $body,
+    sub {
+      my ($body, $headers) = @_;
+      $self->{cv}->end; 
+    };
 }
 
 1;
