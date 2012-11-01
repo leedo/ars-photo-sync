887fa324940c32f5544ff32b4cf4a589c93770af
Lee Aylward <lee@laylward.com>
2012-11-01 18:52:49 -0500

    only open browser with --browser option

--- lib/App/Photosync/Launcher.pm
+++ lib/App/Photosync/Launcher.pm
@@ -14,10 +14,12 @@ sub launch {
   my $s3 = App::Photosync::S3->new(@args{qw/bucket key secret/});
   my $httpd = App::Photosync::HTTPD->new(%args, s3 => $s3);
 
-  my $t; $t = AE::timer 1, 0, sub {
-    undef $t;
-    AnyEvent::Util::run_cmd ["open","http://localhost:$args{port}"];
-  };
+  if ($args{browser}) {
+    my $t; $t = AE::timer 1, 0, sub {
+      undef $t;
+      AnyEvent::Util::run_cmd ["open","http://localhost:$args{port}"];
+    };
+  }
 
   push @s, map {AE::signal $_ => sub {$httpd->shutdown}} qw/INT TERM/;
   return $httpd;
